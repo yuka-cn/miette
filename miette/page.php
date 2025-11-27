1b5087bab36281d60c24d9dc49ecaa390fab113c
yuka <yuka.68cn@gmail.com>
2025-11-28 03:55:09 +0900

    プライバシーポリシー

--- miette/page.php
+++ miette/page.php
@@ -1,15 +1,28 @@
 <?php get_header(); ?>
 
+<?php
+$links = theme_get_links();
+extract($links, EXTR_SKIP);
+?>
+
 <!-- ページコンテンツ -->
-<?php if ( is_page(array('privacypolicy', 'terms-of-service')) ): ?>
+<?php if ( is_page('privacy') ): ?>
   <?php $slug = get_post_field('post_name', get_post()); ?>
-  <div class="page-policy page-policy-layout">
-    <div class="page-policy__body legal legal--<?php echo esc_attr( $slug ); ?>">
+  <div class="policy policy-layout">
+    <div class="policy__body legal legal--<?php echo esc_attr( $slug ); ?>">
       <div class="legal__inner inner">
         <?php the_content(); ?>
       </div>
     </div>
   </div>
 <?php endif; ?>
+
+<!-- お問い合わせ -->
+<div class="policy__button">
+  <a href="<?php echo $contact; ?>" class="button">
+    お問い合わせはこちら
+    <span></span>
+  </a>
+</div>
 
 <?php get_footer(); ?>
