376003ac32b766fa4df2d1ebc88d88d1353e5ee3
yuka <yuka.68cn@gmail.com>
2025-11-29 14:00:58 +0900

    faq修正

--- miette/page-access.php
+++ miette/page-access.php
@@ -1,23 +1,41 @@
 <?php get_header(); ?>
-<!-- ページコンテンツ -->
 
+<?php
+$links = theme_get_links();
+extract($links, EXTR_SKIP);
+?>
+
+<!-- ページコンテンツ -->
 <?php $access_items = SCF::get('access'); ?>
 
-<?php if (empty($access_items)): ?>
-  <p class="access__no-data no-data">
-    ただいま準備中です。<br>公開までしばらくお待ちください。
-  </p>
-<?php else: ?>
-  <table class="table">
-    <tbody>
-      <?php foreach ($access_items as $item): ?>
-        <tr>
-          <th scope="row"><?php echo esc_html($item['access_label']); ?></th>
-          <td><?php echo nl2br(esc_html($item['access_value'])); ?></td>
-        </tr>
-      <?php endforeach; ?>
-    </tbody>
-  </table>
-<?php endif; ?>
+<div class="access access-layout">
+  <div class="access__body">
+    <div class="access__inner inner">
+      <?php if (empty($access_items)): ?>
+        <p class="access__no-data no-data">
+          ただいま準備中です。<br>公開までしばらくお待ちください。
+        </p>
+      <?php else: ?>
+        <div class="access__items">
+          <div class="access__info">
+            <table class="access__table table">
+              <tbody>
+                <?php foreach ($access_items as $item): ?>
+                  <tr>
+                    <th scope="row"><?php echo esc_html($item['access_label']); ?></th>
+                    <td><?php echo nl2br(esc_html($item['access_value'])); ?></td>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+          </div>
+          <div class="access__map">
+
+          </div>
+        </div>
+      <?php endif; ?>
+    </div>
+  </div>
+</div>
 
 <?php get_footer(); ?>
--- miette/page-faq.php
+++ miette/page-faq.php
@@ -13,17 +13,19 @@ $faqs = array_filter(SCF::get('faq'), function($faq){
 ?>
 
 <div class="faq faq-layout">
-  <div class="faq__body faq">
+  <div class="faq__body">
     <div class="faq__inner inner">
       <?php if (empty($faqs)): ?>
         <p class="faq__no-post no-post">ただいま準備中です。<br>公開までしばらくお待ちください。</p>
       <?php else: ?>
-        <?php foreach ($faqs as $faq): ?>
-          <details class="faq__item" open>
-            <summary><?php echo esc_html($faq['question']); ?></summary>
-            <p><?php echo nl2br(esc_html($faq['answer'])); ?></p>
-          </details>
-        <?php endforeach; ?>
+        <div class="faq__items">
+          <?php foreach ($faqs as $faq): ?>
+            <details class="faq__item faq-item" open>
+              <summary><?php echo esc_html($faq['question']); ?></summary>
+              <p><?php echo nl2br(esc_html($faq['answer'])); ?></p>
+            </details>
+          <?php endforeach; ?>
+        </div>
       <?php endif; ?>
     </div>
   </div>  
