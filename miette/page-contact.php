7581236ea5fef729459ce40e8f7365a6395a59dc
yuka <yuka.68cn@gmail.com>
2025-12-04 05:18:58 +0900

    お問い合わせ

--- miette/page-contact.php
+++ miette/page-contact.php
@@ -8,7 +8,7 @@
       <div class="page-contact__error"></div>
       
       <!-- お問い合わせフォーム -->
-      <?php echo do_shortcode('[contact-form-7 id="268ee84" title="お問い合わせ"]'); ?>
+      <?php echo do_shortcode('[contact-form-7 id="bdd0e72" title="お問い合わせ"]'); ?>
     </div>
   </div>
 </main>
