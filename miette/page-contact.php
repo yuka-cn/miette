85fc561a7bc08e09bebd700549c4f258dccd74b8
yuka <yuka.68cn@gmail.com>
2025-12-04 05:25:46 +0900

    クラス名修正

--- miette/page-contact.php
+++ miette/page-contact.php
@@ -1,11 +1,11 @@
 <?php get_header(); ?>
 
 <!-- ページコンテンツ -->
-  <div class="page-contact page-contact-layout">
-    <div class="page-contact__inner inner">
+  <div class="contact contact-layout">
+    <div class="contact__inner inner">
 
       <!-- エラーメッセージ -->
-      <div class="page-contact__error"></div>
+      <div class="contact__error"></div>
       
       <!-- お問い合わせフォーム -->
       <?php echo do_shortcode('[contact-form-7 id="bdd0e72" title="お問い合わせ"]'); ?>
--- miette/page-thanks.php
+++ miette/page-thanks.php
@@ -2,13 +2,13 @@
 
 <!-- ページコンテンツ -->
 
-  <div class="page-contact page-contact-layout">
-    <div class="page-contact__inner inner">
+  <div class="contact contact-layout">
+    <div class="contact__inner inner">
       <!-- 送信完了メッセージ -->
-      <p class="page-contact__thanks-title">
+      <p class="contact__thanks-title">
         お問い合わせ内容を送信完了しました。
       </p>
-      <div class="page-contact__thanks-body">
+      <div class="contact__thanks-body">
         <p>このたびは、お問い合わせ頂き<br>誠にありがとうございます。</p>
         <p>お送り頂きました内容を確認のうえ、<br>3営業日以内に折り返しご連絡いたします。</p>
       </div>
